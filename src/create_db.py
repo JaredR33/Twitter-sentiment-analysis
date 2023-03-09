5a77c41e7a1716d45d7cf8608840536795b944fe
JaredR33 <118567250+JaredR33@users.noreply.github.com>
2023-03-09 23:12:40 +0100

    Finished database

--- src/create_db.py
+++ src/create_db.py
@@ -1,51 +1,77 @@
-#libraries
 import pymysql
-import configparser
+import cryptography
+import pandas as pd
+from sqlalchemy import create_engine
 
-config = configparser.ConfigParser()
-config.read('gpt3app/config.ini')
+username = "admin"
+password = "TheBridgeSchool"
+host = "database-2.cvuovpb4vssk.us-east-2.rds.amazonaws.com" 
+port = 3306
 
+db = pymysql.connect(host = host,
+                     user = username,
+                     password = password,
+                     cursorclass = pymysql.cursors.DictCursor
+)
 
-db = pymysql.connect(host = config['seandatabase']['host'],
-                     user = config['seandatabase']['username'],
-                     password = config['seandatabase']['password'],
-                     port = int(config['seandatabase']['port']),
-                     cursorclass = pymysql.cursors.DictCursor)
+# El objeto cursor es el que ejecutará las queries y devolverá los resultados
 
 cursor = db.cursor()
 
-#Create database
-create_db = '''CREATE DATABASE respuestas_GPT'''
-cursor.execute(create_db)
-cursor.connection.commit()
 
-#Select database and create table
-use_db = ''' USE respuestas_GPT'''
+# Creamos una BD. Tenemos una instancia de MySQL, pero no una BD
+create_db = '''CREATE DATABASE tweets_TheBridge'''
+cursor.execute(create_db)
+
+# Para usar la BD  recien creada
+
+cursor.connection.commit()
+use_db = ''' USE tweets_TheBridge'''
 cursor.execute(use_db)
 
-create_table_users= '''
+# Create two normalized tables, tweets and users
+create_table_users = '''
 CREATE TABLE users (
-    author_id INTEGER PRIMARY KEY
+    author_id INTEGER
 )
 '''
+cursor.execute(create_table_users)
 
-create_table_tweets = '''
+create_table = '''
 CREATE TABLE tweets (
+  author_id INT,
+  created_at TEXT,
   id INT,
-  text TEXT,
-  created_at DATETIME,
+  text INT,
   impression_count INT,
   like_count INT,
   quote_count INT,
   reply_count INT,
-  retweet_count INT,
-  author_id INT,
-  primary key (id),
-  FOREIGN KEY (author_id) REFERENCES users(author_id)
-  )
-'''
+  retweet_count INT
+)
 
-cursor.execute(create_table_users)
-cursor.execute(create_table_tweets)
+'''
+cursor.execute(create_table)
+
+
+#insert data
+
+df = pd.read_csv('../data/tweets_23&23_@TheBridge.csv', index_col=0)
+df_users = pd.DataFrame(df['author_id'])
+
+# create sqlalchemy engine
+
+engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(user = username, pw = password, host = host, db = 'tweets_TheBridge'))
+
+# insertamos todo el dataframe en users
+df_users.to_sql(name='users', con=engine, if_exists= 'append', index=False)
+
+# df into tweets tables
+df.to_sql(name='tweets', con=engine, if_exists= 'append', index=False)
+
 db.commit()
+# Cerrar conexión mysql
 db.close()
+# Cerrar conexión sqlalchuemy
+engine.dispose()
+
